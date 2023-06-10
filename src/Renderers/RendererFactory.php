881b4458c8e3843db15285d8ec6e908c39b9148d
medilies <medilies@gmail.com>
2023-06-10 17:05:48 +0100

    fix RendererFactory

--- src/Renderers/RendererFactory.php
+++ src/Renderers/RendererFactory.php
@@ -3,12 +3,17 @@
 namespace ElaborateCode\RowBloom\Renderers;
 
 use ElaborateCode\RowBloom\RendererContract;
+use ElaborateCode\RowBloom\Types\Css;
+use ElaborateCode\RowBloom\Types\InterpolatedTemplate;
 
 class RendererFactory
 {
     // TODO
-    public function make(): RendererContract
-    {
-        return new HtmlRenderer;
+    public function make(
+        InterpolatedTemplate $template,
+        Css $css,
+        array $config = []
+    ): RendererContract {
+        return new HtmlRenderer($template, $css, $config);
     }
 }

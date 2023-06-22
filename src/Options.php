3188d6463c6d9d3647e0617ca9f76b12fc5fa1d7
medilies <medilies@gmail.com>
2023-06-22 21:57:11 +0100

    options

--- src/Options.php
+++ src/Options.php
@@ -2,20 +2,38 @@
 
 namespace ElaborateCode\RowBloom;
 
+/**
+ * .
+ *
+ * @var array $margin
+ * - Like Css number number,number number,number,number,number.
+ * - Unit in millimeter
+ */
 class Options
 {
     public function __construct(
         public ?int $perPage = null,
-        public string $pageSize = 'A4',
-        public string $pageOrientation = 'P',
-        public int $margins = 1, // in millimeter
-        public ?string $header = null,
-        public ?string $footer = null,
+
+        public string $paperSize = 'A4',
+        public string $layout = 'P',
+
+        public array $margins = [1],
+
+        public ?string $headerLeft = null,
+        public ?string $headerCenter = null,
+        public ?string $headerRight = null,
+
+        public ?string $footerLeft = null,
+        public ?string $footerCenter = null,
+        public ?string $footerRight = null,
+
         public ?int $pageNumbering = null,
+
         public ?string $metadataTitle = null,
         public ?string $metadataAuthor = null,
         public ?string $metadataSubject = null,
         public ?string $metadataKeywords = null,
+
         // security ?
         // compression ?
     ) {

347a00dd52458e440c0b8955e9ddade038a6c1ca
medilies <medilies@gmail.com>
2023-06-28 12:57:12 +0100

    bugfix

--- src/Renderers/PhpChromeRenderer.php
+++ src/Renderers/PhpChromeRenderer.php
@@ -158,8 +158,8 @@ class PhpChromeRenderer implements RendererContract
         // ! style="font-size:10px" needs to be specified on header and footer
 
         if ($this->options->displayHeaderFooter) {
-            $this->phpChromeOptions['headerTemplate'] = $this->options->rawHeader;
-            $this->phpChromeOptions['footerTemplate'] = $this->options->rawFooter;
+            $this->phpChromeOptions['headerTemplate'] = $this->options->rawHeader ?? '';
+            $this->phpChromeOptions['footerTemplate'] = $this->options->rawFooter ?? '';
         }
     }
 

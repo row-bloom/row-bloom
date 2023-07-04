9e868e491ce9ce8be996fa2f6405bb5f30e0cafd
medilies <medilies@gmail.com>
2023-07-04 16:47:03 +0100

    Trying to mock IO

--- tests/unit/DataCollectors/JsonDataCollectorTest.php
+++ tests/unit/DataCollectors/JsonDataCollectorTest.php
@@ -3,15 +3,32 @@
 use ElaborateCode\RowBloom\DataCollectors\DataCollector;
 use ElaborateCode\RowBloom\DataCollectors\DataCollectorFactory;
 use ElaborateCode\RowBloom\DataCollectors\Json\JsonDataCollector;
+use ElaborateCode\RowBloom\Fs\File;
 
 it('parses', function () {
     $dataCollector = app()->make(DataCollectorFactory::class)->make(DataCollector::Json);
 
     expect($dataCollector)->toBeInstanceOf(JsonDataCollector::class);
 
-    // TODO: Mock File
+    $mock = Mockery::mock(File::class);
+
+    $mock->shouldReceive('mustExist')->andReturns($mock);
+    $mock->shouldReceive('mustBeReadable')->andReturns($mock);
+    $mock->shouldReceive('mustBeFile')->andReturns($mock);
+    $mock->shouldReceive('mustBeExtension')->andReturns($mock);
+    $mock->shouldReceive('readFileContent')->andReturns('[
+        {
+            "a": 2,
+            "b": 2
+        },
+        {
+            "a": 3,
+            "b": 3
+        }
+    ]');
+
     expect(
-        $dataCollector->getTable(__DIR__.'/../../stubs/2json.json')->toArray()
+        $dataCollector->getTable($mock)->toArray()
     )->toEqual([
         ['a' => '2', 'b' => '2'],
         ['a' => '3', 'b' => '3'],
--- tests/unit/DataCollectors/Spreadsheets/SpreadsheetDataCollectorTest.php
+++ tests/unit/DataCollectors/Spreadsheets/SpreadsheetDataCollectorTest.php
@@ -9,9 +9,8 @@ it('parses', function () {
 
     expect($dataCollector)->toBeInstanceOf(SpreadsheetDataCollector::class);
 
-    expect(
-        $dataCollector->getTable(__DIR__.'/../../../stubs/1csv.csv')->toArray()
-    )->toEqual([
+    // ! Cannot mock File IO, Phpspreadsheet requires file path and doesn't take a raw string
+    expect($dataCollector->getTable(__DIR__.'/../../../stubs/1csv.csv')->toArray())->toEqual([
         ['a' => '2', 'b' => '2'],
         ['a' => '3', 'b' => '3'],
     ]);

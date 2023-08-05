d6a368b921e9110453506f3b28c09358387974b4
medilies <medilies@gmail.com>
2023-08-05 15:00:00 +0100

    setRendererDrivers

--- src/Support.php
+++ src/Support.php
@@ -3,20 +3,24 @@
 namespace ElaborateCode\RowBloom;
 
 use ElaborateCode\RowBloom\DataCollectors\DataCollector;
+use ElaborateCode\RowBloom\Renderers\Renderer;
 
 class Support
 {
     private array $dataCollectorDrivers = [];
 
+    private array $rendererDrivers = [];
+
     private array $supportedTableFileExtensions = [];
 
     public function __construct()
     {
-        $this->setDataCollectorDrivers();
-        $this->setSupportedTableFileExtensions();
+        $this->setDataCollectorDrivers()
+            ->setSupportedTableFileExtensions()
+            ->setRendererDrivers();
     }
 
-    public function setDataCollectorDrivers(): void
+    public function setDataCollectorDrivers(): static
     {
         foreach (DataCollector::cases() as $dataCollector) {
             $className = $dataCollector->value;
@@ -27,6 +31,8 @@ class Support
 
             $this->dataCollectorDrivers[$dataCollector->name] = $className;
         }
+
+        return $this;
     }
 
     public function getDataCollectorDrivers(): array
@@ -34,11 +40,33 @@ class Support
         return $this->dataCollectorDrivers;
     }
 
-    public function setSupportedTableFileExtensions(): void
+    public function setRendererDrivers(): static
+    {
+        foreach (Renderer::cases() as $renderer) {
+            $className = $renderer->value;
+
+            if (! class_exists($className)) {
+                continue;
+            }
+
+            $this->rendererDrivers[$renderer->name] = $className;
+        }
+
+        return $this;
+    }
+
+    public function getRendererDrivers(): array
+    {
+        return $this->rendererDrivers;
+    }
+
+    public function setSupportedTableFileExtensions(): static
     {
         foreach ($this->dataCollectorDrivers as $className) {
             $this->supportedTableFileExtensions += $className::getSupportedFileExtensions();
         }
+
+        return $this;
     }
 
     public function getSupportedTableFileExtensions(): array

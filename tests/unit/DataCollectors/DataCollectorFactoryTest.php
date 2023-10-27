006c4556f63df4b59d77b16469a252a01e020cb8
medilies <medilies@gmail.com>
2023-10-27 12:04:48 +0100

    test exception case

--- tests/unit/DataCollectors/DataCollectorFactoryTest.php
+++ tests/unit/DataCollectors/DataCollectorFactoryTest.php
@@ -2,7 +2,12 @@
 
 use RowBloom\RowBloom\DataCollectors\DataCollectorFactory;
 use RowBloom\RowBloom\DataCollectors\Json\JsonDataCollector;
+use RowBloom\RowBloom\RowBloomException;
 
-it('factorizes')
+it('makes')
     ->expect(app()->make(DataCollectorFactory::class)->makeFromPath(__DIR__.'/../../stubs/foo.json'))
     ->toBeInstanceOf(JsonDataCollector::class);
+
+it('throws when extension is not supported')
+    ->expect(fn () => app()->make(DataCollectorFactory::class)->makeFromPath(__FILE__))
+    ->throws(RowBloomException::class);

941f0a46d36e2ea3d919f3a8f692eb9b74630799
medilies <medilies@gmail.com>
2023-11-02 15:41:00 +0100

    TablePath -> TableLocation

---
+++ src/Types/TableLocation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RowBloom\RowBloom\Types;
+
+use RowBloom\RowBloom\Fs\File;
+
+class TableLocation
+{
+    public static function make(string $value): static
+    {
+        return new static($value);
+    }
+
+    final public function __construct(
+        public readonly string $value,
+        public readonly ?string $driver = null,
+    ) {
+    }
+
+    public function isFileLocation(): bool
+    {
+        return true;
+    }
+
+    public function getFile(): File
+    {
+        return File::fromPath($this->value);
+    }
+}

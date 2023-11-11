e56654e205fbfb9f7edf9e2e87394969e263fb7a
medilies <medilies@gmail.com>
2023-11-11 18:54:13 +0100

    support driver specific configs

--- src/Config.php
+++ src/Config.php
@@ -2,13 +2,43 @@
 
 namespace RowBloom\RowBloom;
 
+// ? leave chromePath
+// ? validate when passed to RowBloom
+// TODO: rename config property
 class Config
 {
-    public function __construct(
-        public ?string $nodeBinaryPath = null,
-        public ?string $npmBinaryPath = null,
-        public ?string $nodeModulesPath = null,
-        public ?string $chromePath = null,
-    ) {
+    /** @param  array<class-string, object>  $configs*/
+    public function __construct(protected array $configs = [])
+    {
+    }
+
+    public function setConfig(object $config): static
+    {
+        $this->configs[get_class($config)] = $config;
+
+        return $this;
+    }
+
+    /**
+     * @template TClassName
+     *
+     * @param  class-string<TClassName>  $configClassName
+     * @return ?TClassName
+     */
+    public function getConfig($configClassName): ?object
+    {
+        return $this->configs[$configClassName] ?? null;
+    }
+
+    /** @param  class-string  $configClassName */
+    public function mutateConfig($configClassName, callable $callback): static
+    {
+        if (! array_key_exists($configClassName, $this->configs)) {
+            return $this;
+        }
+
+        $callback($this->configs[$configClassName]);
+
+        return $this;
     }
 }

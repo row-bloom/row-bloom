f9c843905d88dd8dddf603f4194075b6f01cd096
medilies <medilies@gmail.com>
2023-12-02 20:51:09 +0100

    BoxArea

--- src/Renderers/Sizing/Length.php
+++ src/Renderers/Sizing/Length.php
@@ -1,26 +1,22 @@
 <?php
 
 /**
- * @see https://www.w3.org/TR/css-values-3
- * @see https://www.w3.org/TR/css-sizing-3
  * @see https://developer.mozilla.org/en-US/docs/Web/CSS/length
- *
- * @see https://www.w3.org/TR/?filter-tr-name=&status%5B%5D=candidateStandard&status%5B%5D=standard&tags%5B%5D=css
+ * @see https://www.w3.org/TR/css-sizing-3
  * @see https://www.w3.org/TR/css-syntax-3
- *
- * TODO: support functional notation. calc(50% - 2em)
  */
 
 namespace RowBloom\RowBloom\Renderers\Sizing;
 
 use RowBloom\RowBloom\RowBloomException;
+use Stringable;
 
-class Length
+class Length implements Stringable
 {
     /**
      * @var (int|float)[][] Absolute units only
      *
-     * ? enum keys
+     * @see https://www.w3.org/TR/css-values-3/#absolute-lengths
      */
     protected const ABSOLUTE_UNIT_EQUIVALENCE = [
         'px' => [
@@ -67,47 +63,34 @@ class Length
         ],
     ];
 
-    public static function fromValue(string $value, LengthUnit $readUnit): static
+    public static function fromString(string $value): static
     {
-        if (is_numeric($value)) {
-            return Length::fromNumber((float)$value, $readUnit);
-        }
+        $value = trim((string) $value);
 
-        return Length::fromString($value, $readUnit);
+        $parsed = static::parseLengthDimension($value);
+
+        return Length::fromNumberUnit($parsed['value'], LengthUnit::from($parsed['unit']));
     }
 
-    public static function fromNumber(float|int $value, LengthUnit $readUnit, LengthUnit $sourceUnit = null): static
+    public static function fromNumberUnit(float|int $value, LengthUnit $unit): static
     {
         if (! is_numeric($value)) {
             throw new RowBloomException("Not numeric value '{$value}'");
         }
 
-        $sourceUnit ??= $readUnit;
-
-        return new static($value, $readUnit, $sourceUnit);
-    }
-
-    public static function fromString(string $value, LengthUnit $readUnit): static
-    {
-        $value = trim((string) $value);
-
-        if (is_numeric($value)) {
-            return Length::fromNumber((float)$value, $readUnit);
-        }
-
-        $parsed = static::parseLengthDimension($value);
-
-        return Length::fromNumber($parsed['value'], LengthUnit::from($parsed['unit']))
-            ->setUnit($readUnit);
+        return new static($value, $unit);
     }
 
     final protected function __construct(
-        private readonly float|int $value,
-        private LengthUnit $readUnit,
-        private readonly LengthUnit $sourceUnit
+        public readonly float|int $value,
+        public readonly LengthUnit $unit,
+        public ?LengthUnit $readUnit = null,
     ) {
-        // TODO: if relative unit require a reference
+        $this->readUnit ??= $this->unit;
     }
+
+    // TODO: setReference
+    // TODO: support functional notation. calc(50% - 2em)
 
     /**
      * @return array{value: float, unit: string}
@@ -116,39 +99,61 @@ class Length
      */
     protected static function parseLengthDimension(string $value): array
     {
+        $value = trim($value);
+
+        if($value === '0') {
+            return ['value' => 0, 'unit' => 'px'];
+        }
+
         $units = implode('|', array_keys(static::ABSOLUTE_UNIT_EQUIVALENCE));
         $regex = "/^(?<value>\d+(\.\d+)?)(?<unit>{$units})$/";
 
         preg_match($regex, $value, $parsed) ?:
-            throw new RowBloomException('Invalid CSS dimension: '.$value);
+            throw new RowBloomException("{Invalid CSS dimension: '{$value}' (must be in <number><unit> format).");
 
         /** @phpstan-ignore-next-line */
         return $parsed;
     }
 
-    public function setUnit(LengthUnit $readUnit): static
+    public function setReadUnit(LengthUnit $readUnit): static
     {
         $this->readUnit = $readUnit;
 
         return $this;
     }
 
-    public function convert(LengthUnit $readUnit): static
+    public function value(LengthUnit $readUnit = null): float
     {
-        return (clone $this)->setUnit($readUnit);
-    }
-
-    public function value(): float
-    {
-        return $this->valueIn($this->readUnit);
-    }
-
-    public function valueIn(LengthUnit $readUnit): float
-    {
-        if ($readUnit === $this->sourceUnit) {
+        if ($this->unit === $this->readUnit & is_null($readUnit)) {
             return $this->value;
         }
 
-        return $this->value * static::ABSOLUTE_UNIT_EQUIVALENCE[$this->sourceUnit->value][$readUnit->value];
+        if (! is_null($readUnit)) {
+            return $this->value *
+                static::ABSOLUTE_UNIT_EQUIVALENCE[$this->unit->value][$readUnit->value];
+        }
+
+        return $this->value *
+            static::ABSOLUTE_UNIT_EQUIVALENCE[$this->unit->value][$this->readUnit->value];
+    }
+
+    public function convert(LengthUnit $readUnit): static
+    {
+        return (clone $this)->setReadUnit($readUnit);
+    }
+
+    public function toFloat(LengthUnit $readUnit = null): float
+    {
+        return $this->value($readUnit);
+    }
+
+    public function toString(LengthUnit $readUnit = null): string
+    {
+        return $this->value($readUnit).($readUnit?->value ?? $this->readUnit->value);
+    }
+
+    public function __toString(): string
+    {
+        return $this->value().$this->readUnit->value;
     }
 }

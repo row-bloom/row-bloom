0b0a8813278f25d8b0a868697548c3d43a14dc30
medilies <medilies@gmail.com>
2023-12-30 16:50:46 +0100

    RowBloom\CssSizing\BoxSize::fromArray

--- src/CssSizing/BoxSize.php
+++ src/CssSizing/BoxSize.php
@@ -8,6 +8,25 @@ class BoxSize
 
     public readonly Length $height;
 
+    public static function new(string|Length $width, string|Length $height): static
+    {
+        return new static($width, $height);
+    }
+
+    /** @param array{width: string, height: string}|array{0: string, 1: string} $size */
+    public static function fromArray(array $size): static
+    {
+        if (! is_string($size['width']) && ! is_string($size[0])) {
+            throw new CssSizingException('Box size as an array must have a width value as string at key "width" or index 0.');
+        }
+
+        if (! is_string($size['height']) && ! is_string($size[1])) {
+            throw new CssSizingException('Box size as an array must have a height value as string at key "height" or index 1.');
+        }
+
+        return new static($size['width'] ?? $size[0], $size['height'] ?? $size[1]);
+    }
+
     final public function __construct(string|Length $width, string|Length $height)
     {
         $this->width = $width instanceof Length ? $width : Length::fromDimension($width);
